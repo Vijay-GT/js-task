72cef29ced8041c2f6a7add27de69570088174be
vijay <bhaskarv819@gmail.com>
2024-07-03 12:46:32 +0530

    js task

---
+++ scripts/index.js
@@ -0,0 +1,83 @@
+var age = "18";
+if (age >= 18) {
+  console.log("You are eligible to vote");
+} else {
+  console.log("You are not eligible to vote");
+}
+
+if (location === "madhapur") {
+  console.log("Welcome to madhapur");
+} else if (location === "borabanda") {
+  console.log("Welcome to borabanda");
+} else {
+  console.log("Welcome to hyderbad");
+}
+
+
+var time = "9am"; 
+
+if (time === "9am") {
+  console.log("Good Morning ");
+} else if (time === "12pm") {
+  console.log("Good AfterNooon");
+} else {
+  console.log("Good Evening");
+} 
+ var x="10"; 
+ 
+if (x > 10) {
+  console.log("x is greater than 10");
+} else {
+  console.log("x is less than or equal to 10");
+} 
+
+var time ="9:30" ;
+if (time === "9:30"){ 
+  console.log("i will go to metro");
+}
+ else{
+  console.log("Otherwise i will go to bus");
+ } 
+
+ var proof ="IDcard";
+ if( proof === "IDcard"){ 
+  console.log("allow to the college");
+ } 
+ else{ 
+  console.log("don't allow the college");
+ } 
+
+ var password = "password@123";
+if (password.length > 8 && password.includes(number) && password.includes(uppercase) && passaword.includes(specialCharcters)
+) 
+ {
+console.log("Strong password");
+} else {
+console.log("Weak password");
+} 
+
+
+var emcetmarks= 40;
+if(emcetmarks >= "40") { 
+
+  console.log( " you are qualify in emcet exam ");
+} 
+else{ 
+  console.log( "you are not qualify in emcet exam")
+} 
+
+
+
+
+ var x = 10;
+if (x > 5 && x < 15) {
+  console.log("x is between 5 and 15");
+} else {
+  console.log("x is not between 5 and 15");
+}
+
+
+
+
+
+
